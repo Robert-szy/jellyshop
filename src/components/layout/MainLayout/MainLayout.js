3a38f6297dbc70c42de2602080bc72694917faf9
Robert Szyszka <robert.szyszka@outlook.com>
2021-09-23 00:15:45 +0200

    Add files (scss, js, redux) in progress

---
+++ src/components/layout/MainLayout/MainLayout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Header from '../Header/Header';
+// import Footer from '../Footer/Footer';
+// import ProductPopup from '../../features/ProductPopup/ProductPopupContainer';
+
+const MainLayout = ({ children }) => (
+  <div>
+    {/* {popupShown && <ProductPopup />} */}
+    <Header />
+    {children}
+    {/* <Footer /> */}
+  </div>
+);
+
+MainLayout.propTypes = {
+  children: PropTypes.node,
+  // popupShown: PropTypes.bool,
+};
+
+export default MainLayout;
